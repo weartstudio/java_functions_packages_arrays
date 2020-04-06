8b03ff00ddaa60e3d795fa2ec742202341af1d2a
weartstudio <ebalazs23@gmail.com>
2020-04-06 13:24:05 +0200

    fibonacci recursive

---
+++ elte/fsz/java/Collatz.java
@@ -0,0 +1,6 @@
+package elte.fsz.java;
+// Write a function (collatz) that accepts an integer parameter called n, and prints out the Collatz-series of n until the element in the series is 1. Collatz.java
+
+public class Collatz {
+
+}
--- elte/fsz/java/FibonacciRec.java
+++ elte/fsz/java/FibonacciRec.java
@@ -6,7 +6,7 @@ public class FibonacciRec {
   public static int fibonacci(int n){
     
     if( n==0 || n==1 ){
-      return 1;
+      return n; // n0 => return 0; n1 => return 1;
     }else{
       return fibonacci(n-1) + fibonacci(n-2);
     }
