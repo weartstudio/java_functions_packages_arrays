0478ebc3dc1ee14600c441940a48548e3b88c867
weartstudio <ebalazs23@gmail.com>
2020-04-06 12:36:47 +0200

    Main file

--- Main.java
+++ Main.java
@@ -1,12 +1,9 @@
 import elte.fsz.java.*;
-
 public class Main{
-
 	public static void main(String[] args) {
 		
 		// 1. Factorial.java
 			System.out.println( "1. factorial(5) = " + Factorial.factorial(5) );
 
 	}
-
 }
